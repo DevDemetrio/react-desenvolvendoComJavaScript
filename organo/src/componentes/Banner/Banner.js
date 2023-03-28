a37c55f363a504056f06c1eefad1b4fa15ce119a
Devdemetrio <devdemetrio@gmail.com>
2023-03-28 06:59:18 -0400

    foi inciada configuração da parte do Banner

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+
+function Banner() {
+  //Essa parte é jsx
+  return (
+    <header className="banner">
+      <img
+        src="/imagens/banner.png"
+        alt="Banner principal da página do Organo"
+      />
+    </header>
+  );
+}
+
+export default Banner;
