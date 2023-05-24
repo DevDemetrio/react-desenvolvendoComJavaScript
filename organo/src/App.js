1be7f0a80f40597b004d79c2484b3f8d25a0bc17
Devdemetrio <devdemetrio@gmail.com>
2023-05-24 19:13:40 -0400

    criado o componente de Time, e foi feita a estilização incial

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,8 +1,47 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Fromulario";
+import Time from "./componentes/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#57c278",
+      corSecundaria: "#D9F7E9",
+    },
+    {
+      nome: "Front-end",
+      corPrimaria: "#82cffa",
+      corSecundaria: "#E8F8FF",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#A6d157",
+      corSecundaria: "#F0F8e2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "#E06b69",
+      corSecundaria: "#FDE7E8",
+    },
+    {
+      nome: "Ux e Design",
+      corPrimaria: "#DB6EBF",
+      corSecundaria: "#FAe9F5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "#FFBAD5",
+      corSecundaria: "#FFF5D9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8429",
+      corSecundaria: "#FFEEDF",
+    },
+  ];
+
   const [colaboradores, setColaboradores] = useState([]);
   const aoNovoColaboradorAdicionado = (coloborador) => {
     setColaboradores([...colaboradores, coloborador]);
@@ -15,6 +54,14 @@ function App() {
           aoNovoColaboradorAdicionado(coloborador)
         }
       />
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
